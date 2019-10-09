bad1a12a756734da88f3941e46057f8f3a7952c9
herlianali <41329931+herlianali@users.noreply.github.com>
2019-10-09 14:11:55 +0700

    Update pkl.sql

--- pkl.sql
+++ pkl.sql
@@ -1,71 +1,36 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 05, 2019 at 06:55 AM
--- Server version: 10.1.40-MariaDB
--- PHP Version: 7.3.5
+-- Adminer 4.7.2 MySQL dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `timeline`;
+CREATE TABLE `timeline` (
+  `id_timeline` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `isi_timeline` text NOT NULL,
+  PRIMARY KEY (`id_timeline`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `timeline_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `pkl`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `photo` varchar(255) NOT NULL DEFAULT 'default.svg'
+  `photo` varchar(255) NOT NULL DEFAULT 'default.svg',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `name`, `photo`) VALUES
-(1, 'elsa', 'elsa.grow@nse-computer.com', '$2y$10$yV0LlH6b0JWWaQ0apl6e/u5YrGDHJfbxkcrl95v0BNs0OCgEU0nK2', 'Elsa', 'default.svg');
+(1,	'elsa',	'elsa.grow@nse-computer.com',	'$2y$10$yV0LlH6b0JWWaQ0apl6e/u5YrGDHJfbxkcrl95v0BNs0OCgEU0nK2',	'Elsa',	'default.svg'),
+(2,	'sadsad',	'sadsad@sadsad.sadsad',	'$2y$10$H9M4khDqySi7grlUPDZbq.EgL5KE5hH41jlFuefz1dXLfaOmZYPjW',	'sadsad',	'default.svg'),
+(3,	'aliyasa',	'herlianali@yahoo.co.id',	'$2y$10$lf9pljLfe6ctd1hgscBI.OIdsl0VMw95QGhBON/L46I7icGr3ZeUO',	'herlian',	'default.svg');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2019-10-09 07:11:35
