5ed89978982c760a413cda49248c1377e5d65329
herlianali <aliyasa@umsida.ac.id>
2019-10-09 12:25:57 +0700

    ganti anjay

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
+$db_user = "wadau";
+$db_pass = "herlianali";
 $db_name = "pkl";
 
-try {    
-    //create PDO connection 
+try {
+    //create PDO connection
     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 } catch(PDOException $e) {
     //show error
-    die("Terjadi masalah: " . $e->getMessage());
+    die("Terjadi masalah gan: " . $e->getMessage());
 }
